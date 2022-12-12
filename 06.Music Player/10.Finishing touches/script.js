f9c1a5351e42542d75092e7ebea92577cda29d6d
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2022-12-12 06:48:00 -0300

    Add files

---
+++ 06.Music Player/10.Finishing touches/script.js
@@ -0,0 +1,77 @@
+const songs = [
+  "Azul da cor do mar - Tim Maia.mp3",
+  "Chove Chuva.mp3",
+  "Eu.mp3",
+  "Gostava Tanto de você.mp3",
+  "Imprevisto.mp3",
+  "Quando bate aquela saudade.mp3",
+];
+const player = document.getElementById("player");
+const progress_bar = document.getElementById('progress');
+
+const createSongList = () => {
+  const list = document.createElement("ol");
+  for (let i = 0; i < songs.length; i++) {
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(songs[i]));
+    list.appendChild(item);
+  }
+  return list;
+};
+
+const songList = document.getElementById("songList");
+songList.appendChild(createSongList());
+const links = document.querySelectorAll("li");
+for (const link of links) {
+  link.addEventListener("click", setSong);
+}
+
+function setSong(e) {
+  document.querySelector("#headphones").classList.remove("pulse");
+
+  const source = document.getElementById("source");
+  source.src = "songs/" + e.target.innerText;
+  document.getElementById(
+    "currentSong"
+  ).innerText = `Now Playing:  ${e.target.innerText}`;
+
+  player.load();
+  player.play();
+
+  document.querySelector("#headphones").classList.add("pulse");
+}
+
+function playAudio() {
+  if (player.readyState) {
+    player.play();
+  }
+}
+
+function pauseAudio() {
+  player.pause();
+}
+
+const slider = document.getElementById("volumeSlider");
+slider.oninput = function (e) {
+  const volume = e.target.value;
+  player.volume = volume;
+};
+
+function updateProgress() {
+  if (player.currentTime > 0) {
+    const progressBar = document.getElementById("progress");
+    progressBar.value = (player.currentTime / player.duration) * 100;
+  }
+}
+
+progress_bar.addEventListener('click', (e) => {
+
+  let totalX = progress_bar.clientWidth;
+  let mouseX = e.offsetX;
+  let song_duration = player.duration;
+  let new_time = parseInt((mouseX * song_duration) / totalX)
+
+  player.currentTime =  new_time;
+  progress_bar.value = (new_time * 100) / song_duration;  
+
+})
---
+++ README.md
@@ -0,0 +1,5 @@
+![Fun Javascript Projects Logo](https://res.cloudinary.com/djn1ticju/image/upload/v1594740008/Fun%20Javascript%20Projects%20Logo.png)
+
+# Sign up for free at [Fun Javascript Projects.com!](https://fun-javascript-projects.com)
+
+## This repo contains completed code samples and assets for [Fun Javascript Projects.com!](https://fun-javascript-projects.com).
